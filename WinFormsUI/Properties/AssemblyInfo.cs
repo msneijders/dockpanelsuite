3d41f5a2729cfe7d8e25cb05546ef0562926602e
Martijn Sneijders <martijn@florisoft.nl>
2020-09-24 11:31:43 +0200

    nuspecs

--- WinFormsUI/Properties/AssemblyInfo.cs
+++ WinFormsUI/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: AssemblyTitle("DockPanel Suite for .Net")]
 [assembly: AssemblyDescription(".NET Docking Library for Windows Forms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Weifen Luo")]
-[assembly: AssemblyProduct("DockPanel Suite")]
+[assembly: AssemblyCompany("Florisoft")]
+[assembly: AssemblyProduct("DockPanel Suite Netcore")]
 [assembly: AssemblyCopyright("Copyright © Weifen Luo and other contributors 2007-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,8 +17,8 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: Guid("9d690ef9-ce19-4c69-874c-e24d8eb36aff")]
 
-[assembly: AssemblyVersion("3.0.6.0")]
-[assembly: AssemblyFileVersion("3.0.6.0")]
+[assembly: AssemblyVersion("3.0.6.1")]
+[assembly: AssemblyFileVersion("3.0.6.1")]
 
 [module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "WeifenLuo.WinFormsUI.Docking", MessageId = "Weifen")]
 [module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "WeifenLuo.WinFormsUI.Docking", MessageId = "Luo")]
